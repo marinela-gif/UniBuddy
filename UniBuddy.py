60cf967fc2f651496e2f8ad4e7f8c19b4203f41e
marinela-gif <ella.berea@gmail.com>
2024-03-08 09:18:31 +0000

    First issue done

--- UniBuddy.py
+++ UniBuddy.py
@@ -1,19 +1,5 @@
 
-#Case Study Story:
-# Imagine the first day of university for a freshman named
-# Alex. Alex is excited but also overwhelmed by the vast campus, numerous courses,
-# and the sea of new faces. To aid new students like Alex, the university's IT
-# department has developed a personalised chatbot. This chatbot, named
-# "UniBuddy", is designed to make the transition smoother for freshmen. Upon
-# accessing the chatbot, Alex is prompted to enter their name, favourite colour, and
-# age. Based on this input, UniBuddy offers a personalised greeting. For instance, if
-# Alex's favourite colour is blue, UniBuddy might suggest joining the university's "Blue
-# Art Club". If Alex is 18, the chatbot might provide information about
-# freshman-specific events. The chatbot also offers a feature where Alex can input
-# specific queries, like "Where is the library?" or "How do I join the debate club?", and
-# UniBuddy responds with relevant information, all while adhering to a friendly tone,
-# using string transformation methods to ensure the responses feel personalised
-# and engaging.
+# UniBuddy chatbot
 
 #print welcome message
 
